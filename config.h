f7087f3583991ddaa8718c523c7b80e37827c5d7
xinantrash <1198629131@qq.com>
2020-04-08 21:59:51 +0800

    2020-04-08

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const unsigned int snap      = 36;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 1;     	/* 0 means no systray */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 22;       /* horiz outer gap between windows and screen edge */
@@ -24,7 +24,7 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "JetBrains Mono Medium:size=18" };
+static const char *fonts[]          = { "JetBrains Mono Medium:size=18", "Noto Sans CJK SC:size=18" };
 static const char dmenufont[]       = "JetBrains Mono Medium:size=24";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
