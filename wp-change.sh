9f45d721434b73bd6274e52f8933b721cf4cffbe
xinantrash <1198629131@qq.com>
2021-03-17 20:43:37 +0800

    03-17

--- wp-change.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/ghibili
-#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
-feh --recursive --randomize --bg-fill ~/wallpapers
